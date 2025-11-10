d84fed82d09cb424dee135cfd0d98c2ac343ec82
Juan Pablo <itjuanpablo>
2025-11-10 18:43:58 -0300

    ADD update do site novamente para inclusao do github actions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
-    const header = document.getElementById('header')
-    // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
+const scrollHeader = () => {
+  const header = document.getElementById('header')
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50 ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -11,17 +11,17 @@ window.addEventListener('scroll', scrollHeader)
 // Função para mudar as imagens na seção Popular (projetos) conforme o clique no arrow
 var swiperPopular = new Swiper(".popular__container", {
 
-      spaceBetween: 32,
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-      loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
 
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*=============== VALUE ACCORDION ===============*/
 const accordionItems = document.querySelectorAll('.value__accordion-item')
@@ -43,10 +43,10 @@ accordionItems.forEach(item => {
 const toggleItem = (item) => {
   const accordionContent = item.querySelector('.value__accordion-content')
 
-  if(item.classList.contains('accordion-open')) {
+  if (item.classList.contains('accordion-open')) {
     accordionContent.removeAttribute('style')
     item.classList.remove('accordion-open')
-  }else {
+  } else {
     accordionContent.style.height = accordionContent.scrollHeight + 'px'
     item.classList.add('accordion-open')
   }
@@ -54,35 +54,35 @@ const toggleItem = (item) => {
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollDown = window.scrollY
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+const scrollActive = () => {
+  const scrollDown = window.scrollY
 
-		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
 }
 window.addEventListener('scroll', scrollActive)
 
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
@@ -104,28 +104,28 @@ if (selectedTheme) {
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    // reset: true
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true
 })
 
 sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
-sr.reveal(`.home__description, .footer__info`, {delay: 500})
-sr.reveal(`.home__search`, {delay: 600})
-sr.reveal(`.home__value`, {delay: 700})
-sr.reveal(`.home__images`, {delay: 800, origin: 'bottom'})
-sr.reveal(`.logos__img`, {interval: 100})
-sr.reveal(`.value__images, .contact__content`, {origin: 'left'})
-sr.reveal(`.value__content, .contact__images`, {origin: 'right'})
+sr.reveal(`.home__description, .footer__info`, { delay: 500 })
+sr.reveal(`.home__search`, { delay: 600 })
+sr.reveal(`.home__value`, { delay: 700 })
+sr.reveal(`.home__images`, { delay: 800, origin: 'bottom' })
+sr.reveal(`.logos__img`, { interval: 100 })
+sr.reveal(`.value__images, .contact__content`, { origin: 'left' })
+sr.reveal(`.value__content, .contact__images`, { origin: 'right' })
